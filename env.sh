49b2f7c1616aed7cad3f7457b20dc73eb7bff910
Cora Iberkleid <ciberkleid@gmail.com>
2020-04-26 18:49:26 -0400

    independent scripts and files args

--- env.sh
+++ env.sh
@@ -1,12 +1,9 @@
 # Usage example:
-# source env.sh scripts/dockerfile.txt
-# source demorunner.sh scripts/dockerfile.txt
+# source env.sh scripts/dockerfile-1.txt files/dockerfile
+# source demorunner.sh scripts/dockerfile-1.txt
 
 demo_script=""
-# default color is yellow. Choices are yellow or blue
-export DEMO_COLOR=blue
-# Default delay is 10. To make it faster, increase the number
-export DEMO_DELAY=15
+demo_files=""
 
 if [ ! -f "${1}" ]; then
   echo "File does not exist: [${1}]"
@@ -15,19 +12,30 @@ else
   demo_script="${1}"
 fi
 
+if [ ! -d "${2}" ]; then
+  echo "Directory does not exist: [${2}]"
+  kill -INT $$
+else
+  demo_files="${2}"
+fi
+
 ##### ENV VARS
+
+# default color is yellow. Choices are yellow or blue
+export DEMO_COLOR=blue
+# Default delay is 10. To make it faster, increase the number
+export DEMO_DELAY=15
+export SAVED_DEMO_DELAY=${DEMO_DELAY}
 
 # brew install coreutils (for greadlink)
 demo_script_absolute_path=$(greadlink -f "${demo_script}")
-demo_script_dirname=$(echo "${demo_script_absolute_path}" | sed 's:/[^/]*$::')
-demo_script_parent_dirname=$(echo "${demo_script_dirname}" | sed 's:/[^/]*$::')
-demo_script_basename=$(basename "${demo_script}")
-demo_script_nickname=$(echo "${demo_script_basename}" | cut -d. -f1)
+demo_files_absolute_path=$(greadlink -f "${demo_files}")
+demo_script_handle=$(echo $(basename "${demo_script}") | cut -d. -f1)
 
 export DEMO_HOME=`pwd`
-export DEMO_TEMP="${DEMO_HOME}/temp/${demo_script_nickname}"
-export DEMO_SCRIPT="${demo_script_dirname}/${demo_script_basename}"
-export DEMO_FILES="${demo_script_parent_dirname}/files/${demo_script_nickname}"
+export DEMO_SCRIPT="${demo_script_absolute_path}"
+export DEMO_FILES="${demo_files_absolute_path}"
+export DEMO_TEMP="${DEMO_HOME}/temp/${demo_script_handle}"
 
 echo
 echo "### Setting env vars"
@@ -37,14 +45,18 @@ echo "DEMO_SCRIPT=${DEMO_SCRIPT}"
 echo "DEMO_FILES=${DEMO_FILES}"
 echo "DEMO_COLOR=${DEMO_COLOR}"
 echo "DEMO_DELAY=${DEMO_DELAY}"
-echo "DEMO_DELAY=${DEMO_DELAY}"
+echo "SAVED_DEMO_DELAY=${SAVED_DEMO_DELAY}"
 echo
 
 ##### TEMP DIR
 
-if [ -d "${DEMO_TEMP}" ]; then
-  read -p "Temp dir exits. Keep or remove? [KP|rm] : " action
-  action=${action:-KP}
+if [ "$(ls -A ${DEMO_TEMP})" ]; then
+  echo "Temp dir is not empty [${DEMO_TEMP}]"
+  echo "Contents:"
+  ls -la "${DEMO_TEMP}"
+  echo
+  read -p "Keep or remove? [KP|rm] : " action
+  action="${action:-KP}"
   if [[ "${action}" =~ rm|RM|Rm|rM ]]; then
     echo "Removing temp directory ${DEMO_TEMP}"
     rm -rf "${DEMO_TEMP}"
